f55a702ad6085ab8188e91f0c5e4de3c387d8368
wkx <359518081@qq.com>
2019-05-22 16:22:20 +0800

    init commit

---
+++ src/main/java/com/wkx/study/ArrayList.java
@@ -0,0 +1,241 @@
+package com.wkx.study;
+
+import java.util.*;
+
+public class ArrayList<E> extends AbstractList<E> implements List<E> {
+
+    private int size;
+
+    private Object[] data;
+
+    private static final Object[] EMPTY_ARRAY = {};
+
+    private static final int INIT_CAPACITY = 10;
+
+    public ArrayList() {
+        this.data =  EMPTY_ARRAY;
+    }
+
+    public ArrayList(int capacity) {
+        if (capacity == 0) {
+            data = EMPTY_ARRAY;
+        } else {
+            data = new Object[capacity];
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Itr();
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] objectArray = new Object[size];
+        System.arraycopy(data, 0, objectArray, 0, size);
+        return objectArray;
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        if (a.length > size) {
+            for (int i = size; i < a.length; i++) {
+                a[i] = null;
+            }
+        }
+        System.arraycopy(data, 0, a, 0, a.length);
+        return a;
+    }
+
+    @Override
+    public boolean add(E e) {
+        ensureLength(size + 1);
+        data[size ++] = e;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        int index;
+        if ((index = indexOf(o)) >= 0) {
+            remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        if (size < c.size()) {
+            return false;
+        }
+        for (Object o : c) {
+            if (!contains(o)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        ensureLength(c.size() + size);
+        for (E e : c) {
+            data[size ++ ] = e;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        ensureLength(c.size() + size);
+        System.arraycopy(data, index, data, size, c.size());
+        Object[] objects = c.toArray();
+        for (int i = index; i < objects.length; i++) {
+            data[index++] = objects[i];
+        }
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        int index;
+        for (Object o : c) {
+            if ((index = indexOf(o)) > 0) {
+                remove(index);
+            }
+        }
+        size -= c.size();
+        return true;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        Object[] tempArr = new Object[c.size()];
+        int i = 0;
+        for (Object o : c) {
+            int index = indexOf(o);
+            if (index != -1) {
+                tempArr[i ++] = data[index];
+            }
+        }
+        data = tempArr;
+        size = tempArr.length;
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = data.length - 1; i >= 0; i--) {
+            data[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public E get(int index) {
+        checkRange(index);
+        @SuppressWarnings("unchecked")
+        E e = (E) data[index];
+        return e;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        checkRange(index);
+        @SuppressWarnings("unchecked")
+        E e = (E) data[index];
+        data[index] = element;
+        return e;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        ensureLength(size + 1);
+        System.arraycopy(data, index, data, index+1, size - index + 1);
+        data[index] = element;
+        size ++;
+    }
+
+    @Override
+    public E remove(int index) {
+        checkRange(index);
+        @SuppressWarnings("unchecked")
+        E e = (E) data[index];
+        System.arraycopy(data, index + 1, data, index, size- index - 1);
+        size--;
+        data[size] = null;
+        return e;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        for (int i = 0; i < data.length; i++) {
+            if (Objects.equals(data[i], o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        for (int i = data.length - 1; i >= 0; i--) {
+            if (Objects.equals(data[i], o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+
+
+    private void checkRange(int index) {
+        if (index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private void ensureLength(int miniCapacity) {
+        if (miniCapacity > data.length) {
+            int nextCapacity = data == EMPTY_ARRAY ? INIT_CAPACITY : size + (size >> 1);
+            nextCapacity =  Math.max(nextCapacity, miniCapacity);
+            Object[] tempArray = new Object[nextCapacity];
+            System.arraycopy(data, 0, tempArray, 0, size);
+            data = tempArray;
+        }
+    }
+
+    public  class Itr implements Iterator<E> {
+
+        private int cursor = 0;
+
+        @Override
+        public boolean hasNext() {
+            return cursor < size;
+        }
+
+        @Override
+        public E next() {
+            @SuppressWarnings("unchecked")
+            E e = (E) data[cursor++];
+            return e;
+        }
+    }
+}
---
+++ src/test/java/com/wkx/study/ArrayListTest.java
@@ -0,0 +1,190 @@
+package com.wkx.study;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ArrayListTest {
+
+    private ArrayList<String> arrayList = new ArrayList<>();
+
+    private static final String[] STR_ARR = new String[100];
+
+    @Before
+    public void before() {
+        System.out.println("start");
+        for (int i = 0; i < STR_ARR.length; i++) {
+            STR_ARR[i] = i + "";
+        }
+        for (int i = 0; i < 10; i++) {
+            arrayList.add((STR_ARR[i]));
+        }
+    }
+
+    @After
+    public void after() {
+        arrayList.forEach(System.out::println);
+        System.out.println("end");
+    }
+
+    @Test
+    public void isEmpty() {
+        assertFalse(arrayList.isEmpty());
+    }
+
+    @Test
+    public void contains() {
+        for (int i = 0; i < 10; i++) {
+            assertTrue(arrayList.contains(STR_ARR[i]));
+        }
+    }
+
+    @Test
+    public void iterator() {
+        Iterator<String> iterator = arrayList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    @Test
+    public void toArray() {
+        Object[] objects = arrayList.toArray();
+        for (int i = 0; i < objects.length; i++) {
+            assertEquals(STR_ARR[i], objects[i]);
+        }
+    }
+
+    @Test
+    public void toArray1() {
+        String[] integers1 = new String[10];
+        String[] integers2 = arrayList.toArray(integers1);
+        assertArrayEquals(integers1, integers2);
+        integers1 = new String[3];
+        integers2 = arrayList.toArray(integers1);
+        assertArrayEquals(integers1, integers2);
+    }
+
+    @Test
+    public void add() {
+        arrayList.add(STR_ARR[11]);
+        assertEquals(arrayList.get(arrayList.size() - 1) , STR_ARR[11]);
+    }
+
+    @Test
+    public void remove() {
+        arrayList.remove(STR_ARR[3]);
+        assertFalse(arrayList.contains(STR_ARR[3]));
+    }
+
+    @Test
+    public void containsAll() {
+        List<String> temp = new ArrayList<>(10);
+        for (int i = 0; i < 9; i++) {
+            temp.add(STR_ARR[i]);
+        }
+        assertTrue(arrayList.containsAll(temp));
+        temp.add(STR_ARR[20]);
+        assertFalse(arrayList.containsAll(temp));
+    }
+
+    @Test
+    public void addAll() {
+        List<String> temp = new ArrayList<>(10);
+        for (int i = 10; i < 20; i++) {
+            temp.add(STR_ARR[i]);
+        }
+        arrayList.addAll(temp);
+        assertTrue(arrayList.containsAll(temp));
+    }
+
+    @Test
+    public void addAll1() {
+        List<String> temp = new ArrayList<>(10);
+        for (int i = 10; i < 20; i++) {
+            temp.add((STR_ARR[i]));
+        }
+        arrayList.addAll(2, temp);
+        assertFalse(arrayList.containsAll(temp));
+    }
+
+    @Test
+    public void removeAll() {
+        int i;
+        List<String> temp = new ArrayList<>(10);
+        for (i = 0; i < 5; i++) {
+            arrayList.remove(STR_ARR[i]);
+            temp.add(STR_ARR[i]);
+        }
+        assertFalse(arrayList.containsAll(temp));
+        for (; i < 10; i++) {
+            arrayList.remove(STR_ARR[i]);
+            temp.add(STR_ARR[i]);
+        }
+        assertFalse(arrayList.containsAll(temp));
+        assertTrue(arrayList.isEmpty());
+    }
+
+    @Test
+    public void retainAll() {
+        List<String> temp = new ArrayList<>(10);
+        for (int i = 0; i < 5; i++) {
+            temp.add(STR_ARR[i]);
+        }
+        arrayList.retainAll(temp);
+        assertArrayEquals(temp.toArray(), arrayList.toArray());
+    }
+
+    @Test
+    public void clear() {
+        arrayList.clear();
+        assertFalse(arrayList.isEmpty());
+    }
+
+    @Test
+    public void get() {
+        for (int i = 0; i < 10; i++) {
+            assertEquals(STR_ARR[i], STR_ARR[i]);
+        }
+    }
+
+    @Test
+    public void set() {
+        arrayList.set(3,STR_ARR[99]);
+        assertTrue(arrayList.get(3).equals(STR_ARR[99]));
+    }
+
+    @Test
+    public void add1() {
+        arrayList.add(3,STR_ARR[99]);
+        assertEquals(arrayList.get(3), STR_ARR[99]);
+    }
+
+    @Test
+    public void remove1() {
+        arrayList.remove(STR_ARR[4]);
+        assertFalse(arrayList.contains(STR_ARR[4]));
+    }
+
+    @Test
+    public void indexOf() {
+        for (int i = 0; i < 10; i++) {
+            assertEquals(arrayList.indexOf(STR_ARR[i]) , i);
+        }
+    }
+
+    @Test
+    public void lastIndexOf() {
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(STR_ARR[i]);
+        }
+        for (int i = 0; i < 10; i++) {
+            assertTrue(arrayList.lastIndexOf(STR_ARR[i]) == i + 10);
+        }
+    }
+}
